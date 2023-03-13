b4afa6bba15e11a883212f307846f366a27efbe0
Exxellent <mikimiki55550@gmail.com>
2023-03-13 17:34:01 +0300

    mot

--- web/app/models.py
+++ web/app/models.py
@@ -7,27 +7,25 @@ from flask_login import UserMixin
 
 from app import db, app
 
-class Train(db.Model, UserMixin):  
-    __tablename__ = 'trains'
+class Product(db.Model, UserMixin):  
+    __tablename__ = 'products'
 
     id = db.Column(db.Integer, primary_key=True)    
-    departure_station = db.Column(db.String(40), nullable=False)
-    arrival_station = db.Column(db.String(250), nullable=False)
-    time_departure = db.Column(db.String(100), nullable=False)
-    time_arrival = db.Column(db.String(100), nullable=False)
+    name_product = db.Column(db.String(40), nullable=False)
+    price = db.Column(db.String(250), nullable=False)
+    desc = db.Column(db.String(100), nullable=False)
 
-
-class Ticket(db.Model, UserMixin):  
-    __tablename__ = 'tickets'
+class Orders(db.Model, UserMixin):  
+    __tablename__ = 'orders'
 
     id = db.Column(db.Integer, primary_key=True)    
-    full_name = db.Column(db.String(40), nullable=False)
-    passport_series = db.Column(db.String(250), nullable=False)
-    number_train = db.Column(db.String(100), nullable=False)
-    seat_number = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(40), nullable=False)
+    prod_in_order = db.Column(db.String(250), nullable=False)
+    count = db.Column(db.String(100), nullable=False)
+    
     
     def check_password(self, password: str):
-        return self.passport_series == password
+        return self.password == password
 
 
     
